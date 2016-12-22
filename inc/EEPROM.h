1f6066d2531811e4689a21fca371fbe7dc016205
Romeli <Romeli@Romeli-PC.lan>
2016-12-22 14:04:13 +0800

    111

---
+++ inc/EEPROM.h
@@ -0,0 +1,16 @@
+/*
+ * EEPROM.h
+ *
+ *  Created on: 2016Äê12ÔÂ7ÈÕ
+ *      Author: Romeli
+ */
+
+#ifndef EEPROM_H_
+#define EEPROM_H_
+
+class EEPROMClass {
+};
+
+extern EEPROMClass EEPROM;
+
+#endif /* EEPROM_H_ */
---
+++ src/EEPROM.cpp
@@ -0,0 +1,9 @@
+/*
+ * EEPROM.cpp
+ *
+ *  Created on: 2016Äê12ÔÂ7ÈÕ
+ *      Author: Romeli
+ */
+
+#include "EEPROM.h"
+#include "U_I2C1.h"
